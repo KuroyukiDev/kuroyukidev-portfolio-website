e5b69372732d5d124eccca3b39350b8af6bed0e6
KuroyukiDev <kuroyuki.dev@gmail.com>
2019-09-07 12:06:54 -0700

    Update: Add projects to List DB && Edit Grammar in Summary text

--- public/projects_lib.js
+++ public/projects_lib.js
@@ -1,49 +1,79 @@
 module.exports.projects = {
-  reactAppsList: [
-    {
-      name: "Expensify App",
-      slug: "Expensify",
-      index: 0,
-      appDesc: "A personal expenses manager app for my portfolio.",
-      githubRepoUrl: "https://github.com/KuroyukiDev/expensify-app",
-      appUrl: "http://ky-expensify.herokuapp.com/",
-      id: "/category/react/expensify"
-    }
-  ],
-  nodejsAppsList: [
-    // {
-    //   name: "Expensify App",
-    //   slug: "Expensify2",
-    //   index: 0,
-    //   appDesc: "A personal expenses manager app for my portfolio.",
-    //   githubRepoUrl: "https://github.com/KuroyukiDev/expensify-app",
-    //   appUrl: "http://ky-expensify.herokuapp.com/",
-    //   id: "/category/nodejs/expensify"
-    // },
-    // {
-    //   name: "Temp Title",
-    //   slug: "TempTitle2",
-    //   index: 1,
-    //   appDesc: "A temporary placeholder app description. The Repo Link is to a Live Repo though.",
-    //   githubRepoUrl: "https://github.com/KuroyukiDev/expensify-app",
-    //   appUrl: "http://ky-expensify.herokuapp.com/",
-    //   id: "/category/nodejs/temp-url"
-    // }
-  ],
-  otherAppsList: []
+	reactAppsList: [
+		{
+			name: "Expensify App",
+			slug: "Expensify",
+			index: 0,
+			appDesc: "A personal expenses manager app for my portfolio.",
+			githubRepoUrl: "https://github.com/KuroyukiDev/expensify-app",
+			appUrl: "http://ky-expensify.herokuapp.com/",
+			id: "/category/react/expensify"
+		},
+		{
+			name: "Hacker Next",
+			slug: "HackerNext",
+			index: 1,
+			appDesc: "A Hacker News Clone made with Next.js",
+			githubRepoUrl: "https://github.com/KuroyukiDev/ky-hacker-next",
+			appUrl: "http://ky-hacker-next.herokuapp.com/",
+			id: "/category/react/hackernext"
+		}
+	],
+	nodejsAppsList: [
+		{
+			name: "CLI OSRS Toolkit",
+			slug: "OSRSToolkit",
+			index: 0,
+			appDesc: "A Node.js CLI Toolkit for players of Old School RuneScape. Runs locally via the" +
+				" CLI. (NOTE:: Requires Node.js to be installed locally.)",
+			githubRepoUrl: "https://github.com/KuroyukiDev/nodejs-cli-osrs-toolkit",
+			appUrl: "https://www.dropbox.com/s/7txqz2u6007ival/nodejs-cli-osrs-toolkit-master.zip?dl=0",
+			id: "/category/nodejs/osrstoolkit"
+		},
+		// {
+		//   name: "Temp Title",
+		//   slug: "TempTitle2",
+		//   index: 1,
+		//   appDesc: "A temporary placeholder app description. The Repo Link is to a Live Repo
+		// though.", githubRepoUrl: "https://github.com/KuroyukiDev/expensify-app", appUrl:
+		// "http://ky-expensify.herokuapp.com/", id: "/category/nodejs/temp-url" }
+	],
+	otherAppsList: [
+		{
+			name: "CLI OSRS Toolkit",
+			slug: "OSRSToolkit",
+			index: 0,
+			appDesc: "A Node.js CLI Toolkit for players of Old School RuneScape. Runs locally via the" +
+				" CLI. (NOTE:: Requires Node.js to be installed locally.)",
+			githubRepoUrl: "https://github.com/KuroyukiDev/nodejs-cli-osrs-toolkit",
+			appUrl: "https://www.dropbox.com/s/7txqz2u6007ival/nodejs-cli-osrs-toolkit-master.zip?dl=0",
+			id: "/category/other/osrstoolkit"
+		},
+		{
+			name: "Recipe Book",
+			slug: "KYRecipeBookAngularApp",
+			index: 1,
+			appDesc: "(Angular App) Manage your recipes online and create a shopping list for ingredients you need" +
+        " for the recipes. (NOTE:: Project temporarily mothballed, but most of the features are" +
+        " fully working.)",
+			githubRepoUrl: "https://github.com/KuroyukiDev/kydev-recipe-book-app",
+			appUrl: "http://kydev-recipe-book.herokuapp.com/",
+			id: "/category/other/kyrecipebookangularapp"
+		},
+	]
 };
 
 module.exports.mainData = {
-  twitterProfile: {
-    profileUrl: "https://twitter.com/KuroyukiDev",
-    btnIconUrl: "/public/images/twitter-btn.png"
-  },
-  patreonProfile: {
-    profileUrl: "https://www.patreon.com/KuroyukiDev",
-    btnIconUrl: "/public/images/patreon-btn.png"
-  },
-  itchioProfile: {
-    profileUrl: "https://kuroyukidev.itch.io/",
-    btnIconUrl: "/public/images/itchio-btn-sm.png"
-  }
+	twitterProfile: {
+		profileUrl: "https://twitter.com/KuroyukiDev",
+		btnIconUrl: "/public/images/twitter-btn.png"
+	},
+	patreonProfile: {
+		profileUrl: "https://www.patreon.com/KuroyukiDev",
+		btnIconUrl: "/public/images/patreon-btn.png"
+	},
+	itchioProfile: {
+		profileUrl: "https://kuroyukidev.itch.io/",
+		btnIconUrl: "/public/images/itchio-btn-sm.png"
+	}
 };
